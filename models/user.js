cda4f74d4405a109fcd2f09e6334f3fcc18b596f
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-11-14 13:09:35 +0600

    Implemented joi validation function for user input.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 
 const userSchema = Schema({
   email: {
@@ -26,4 +27,14 @@ userSchema.methods.generateJWT = function () {
   return token;
 };
 
+const validateUser = (user) => {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+};
+
 module.exports.User = model("User", userSchema);
+module.exports.validate = validateUser;
