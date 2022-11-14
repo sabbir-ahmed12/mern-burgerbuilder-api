d0bfb33666ba5007e78c525b157141881dad971a
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-11-14 13:31:09 +0600

    Created router for new user registration.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const userRouter = require("./routers/userRouter");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/user", userRouter);
 
 module.exports = app;
