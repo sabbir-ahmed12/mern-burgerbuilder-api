975c57af3c077e61f396bc179162285fe004bb3d
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-11-14 16:58:27 +0600

    Added authorization to order route.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routers/userRouter");
+const orderRouter = require("./routers/orderRouter");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/order", orderRouter);
 
 module.exports = app;
---
+++ models/order.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+
+const orderSchema = Schema({
+  userId: Schema.Types.ObjectId,
+  ingredients: [{ type: { type: String }, amount: Number }],
+  customer: {
+    deliveryAddress: String,
+    phone: String,
+    paymentType: String,
+  },
+  price: Number,
+  orderTime: { type: Date, default: Date.now() },
+});
+
+module.exports.Order = model("order", orderSchema);
